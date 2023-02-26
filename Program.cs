6eb60938c878f6760f0185836a1823d58551c026
andulica <75125226+andulica@users.noreply.github.com>
2023-02-26 09:53:12 +0000

    Added a few comments where I thought they were needed for a clearer picture. Changed a few messages for a better understanding of what the player should do next. Issue #8 solved. Created a ArrayList that stores all chars and created new condition in first if statement of the while loop to check if the char entered was previously entered. If not , the char is added to both the secretWord array and guessedChar arraylist.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace HangManExercise
+﻿using System.Collections;
+
+namespace HangManExercise
 {
     internal class Program
     {
@@ -11,15 +13,11 @@
             int counter = 0;
             Random randNumber = new Random();
             int randNumberChosen = randNumber.Next(0, wordsToChoose.Length);
-
-           
+          
             string pickedWord = wordsToChoose[randNumberChosen];
 
-
             char[] secretWord = new char[pickedWord.Length];
-
-
-
+            
             for (int i = 0; i < pickedWord.Length; i++)
             {
                 secretWord[i] = '_';
@@ -30,30 +28,42 @@
                 Console.Write(" " + underscore + " ");
             }
 
+            var guessedChars = new ArrayList();
 
             while (secretWord.Contains('_') && counter < MAX_CHANCES)
             {
 
                 Console.WriteLine("Please guess a letter from the word: ");
                 char playerGuess = Console.ReadKey().KeyChar;
+                
                 Console.WriteLine();
 
-
-                if (pickedWord.Contains(playerGuess))
+                // Checks if the character chose by the player is in the secret word by looping through all the array and if it is
+                // it will be added at the right location in the array of secretWord
+                if (pickedWord.Contains(playerGuess) && !guessedChars.Contains(playerGuess))
                 {
                     for (int i = 0; i < pickedWord.Length; i++)
                     {
                         if (playerGuess == pickedWord[i])
                         {
                             secretWord[i] = playerGuess;
+                            guessedChars.Add(playerGuess);
                         }
                         Console.Write(" " + secretWord[i] + " ");
+                                             
                     }
+                    
                 }
+
+                else if (guessedChars.Contains(playerGuess))
+                {
+                    Console.WriteLine("You already guessed this letter!");
+                }
+                               
                 else
                 {
                     counter++;
-                    Console.WriteLine("You have left " + (MAX_CHANCES - counter) + " chances.");
+                    Console.WriteLine("Wrong answer! You have left " + (MAX_CHANCES - counter) + " chances.");
                 }
             }
 
